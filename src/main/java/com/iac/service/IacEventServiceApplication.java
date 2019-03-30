fdf1cd638e02e4f87b3a0722e00fb3433b974c87
mcordones <miguelcordones@northwesternmutual.com>
2019-03-30 09:21:21 -0400

    initial commit, setting IAC api service  maven project

---
+++ src/main/java/com/iac/service/IacEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iac.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IacEventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IacEventServiceApplication.class, args);
+    }
+
+}
